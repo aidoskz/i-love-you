d14374d99c9279f51b8c84e6ca76d2f6744a81d0
Aidos <aidos.kakimzhan@gmail.com>
2019-09-25 12:56:08 +0600

    fix message

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 const os = require('os');
-console.log(os.userInfo().username==='root'?'I love you too! 😘':"I've a boyfriend 😌");
+console.log(os.userInfo().username==='root'?'I love you too! 😘':"I have a boyfriend 😌");
